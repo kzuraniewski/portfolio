15e87f8f4dfa7a4ddfaf00b3e3205746e53d7bbc
nick-here <zuraniewski.karol@gmail.com>
2022-02-25 10:24:09 +0100

    cursor functionality + styling

---
+++ src/components/Cursor.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useRef, useState } from 'react';
+import classNames from 'classnames';
+
+const DELAY = 5;
+
+const Cursor = () => {
+	const cursor = useRef(null);
+	const cursorSmooth = useRef(null);
+	const mousePos = useRef({ pageX: 0, pageY: 0 });
+	const [hover, setHover] = useState(false);
+
+	const moveSmoothCursor = () => {
+		const style = cursorSmooth.current.style;
+		const { pageX, pageY } = mousePos.current;
+		const topParsed = parseFloat(style.top),
+			leftParsed = parseFloat(style.left);
+
+		style.top = `${topParsed + (pageY - topParsed) / DELAY}px`;
+		style.left = `${leftParsed + (pageX - leftParsed) / DELAY}px`;
+
+		requestAnimationFrame(moveSmoothCursor);
+	};
+
+	useEffect(() => {
+		document.addEventListener('mousemove', ({ pageX, pageY }) => {
+			// update normal cursor's position
+			cursor.current.style.top = `${pageY}px`;
+			cursor.current.style.left = `${pageX}px`;
+
+			// update mousePos for smooth movement updates
+			mousePos.current = { pageX, pageY };
+		});
+
+		// update smooth cursor's position
+		requestAnimationFrame(moveSmoothCursor);
+
+		// add hover effect for given elements
+		['a', 'btn'].forEach(query =>
+			document.querySelectorAll(query).forEach(el => {
+				el.addEventListener('mouseenter', () => setHover(true));
+				el.addEventListener('mouseleave', () => setHover(false));
+			})
+		);
+	}, []);
+
+	return (
+		<>
+			<div ref={cursor} className={classNames('cursor', { 'cursor--hover': hover })} />
+			<div
+				ref={cursorSmooth}
+				style={{ top: '0px', left: '0px' }}
+				className={classNames('cursor cursor--smooth', { 'cursor--hover': hover })}
+			/>
+		</>
+	);
+};
+
+export default Cursor;
