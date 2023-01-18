adcd0329c681b9f604b87f50b1a012143e56fc3e
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2023-01-18 23:39:45 +0100

    config updated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,15 +6,6 @@ module.exports = {
 			sans: ['Graphik', 'sans-serif'],
 			serif: ['Merriweather', 'serif'],
 		},
-		extend: {
-			spacing: {
-				128: '32rem',
-				144: '36rem',
-			},
-			borderRadius: {
-				'4xl': '2rem',
-			},
-		},
 	},
 	plugins: [],
 };
