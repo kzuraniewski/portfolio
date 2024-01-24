4b352ffc1e41f5decda08875aebda14e708cfffb
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-01-24 17:56:29 +0100

    redundant components removed

--- src/components/Paper.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-export type PaperProps = PropsWithChildren<{}>;
-
-export default function Paper({ children }: PaperProps) {
-	return <div className="p-3 bg-white shadow-sm">{children}</div>;
-}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,2 @@
-export { default as Header } from './Header';
-export { default as HeaderLink } from './HeaderLink';
-export { default as Link } from './Link';
-export { default as Tabs } from './Tabs';
-export { default as Tab } from './Tab';
-export { default as Paper } from './Paper';
-export { default as Skills } from './Skills';
 export { default as ContactMe } from './ContactMe';
+export { default as Header } from './Header';
