42a0dac0753c5f5f7520aeb12a550448f5c27a3e
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-01-28 21:21:02 +0100

    redundant code removed

--- src/lib/context.ts
+++
@@ -1,7 +0,0 @@
-import createFastContext from './createFastContext';
-
-const { Provider: AppContextProvider, useStore } = createFastContext({
-	currentSectionIndex: 0,
-});
-
-export { AppContextProvider, useStore };
