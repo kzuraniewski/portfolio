f3bd99b7b132dcb4bad0b31c6dd0964a244e797f
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-01-29 00:26:36 +0100

    tech stack ui updated

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,10 +1,18 @@
+import { BiLogoJava } from 'react-icons/bi';
+import {
+	SiMui,
+	SiNodedotjs,
+	SiReact,
+	SiTypescript,
+	SiWordpress,
+} from 'react-icons/si';
 import { Section } from '../components/Section';
 
 export default function LandingPage() {
 	return (
 		<Section title="About me" id="about">
-			<div className="flex items-center">
-				<p className="w-1/2 mb-5">
+			<div className="flex flex-col items-center">
+				<p className="w-2/3 mb-20 text-center">
 					My name is Karol and I develop web stuff. My web development
 					journey started in 2021 - when I got to know React. Since
 					then I've had the opportunity to design, develop and
@@ -13,22 +21,31 @@ export default function LandingPage() {
 					.
 				</p>
 
-				<fieldset className="px-10 mx-auto border-2 border-dashed rounded py-7 w-fit border-secondary">
-					<legend className="px-3 mx-auto font-bold uppercase">
-						Technologies I've used
-					</legend>
+				<h3 className="px-3 mx-auto font-bold uppercase mb-14">
+					Technologies I've worked with
+				</h3>
 
-					<ul className="list-['>'] marker:text-accent grid grid-cols-2 gap-y-2 gap-x-10">
-						{skills.map((skill) => (
-							<li className="px-3 w-max">
-								<span className="-ml-2">{skill}</span>
+				<ul className="flex items-center justify-between w-5/6">
+					{[
+						<SiTypescript />,
+						<SiReact />,
+						<SiMui />,
+						<BiLogoJava />,
+						<SiNodedotjs />,
+						<SiWordpress />,
+					].map((icon, index, array) => (
+						<>
+							<li className="flex flex-col items-center gap-6">
+								<span className="text-4xl">{icon}</span>
 							</li>
-						))}
-					</ul>
-				</fieldset>
+
+							{index !== array.length - 1 && (
+								<div className="h-10 border-r-2 border-dashed border-secondary" />
+							)}
+						</>
+					))}
+				</ul>
 			</div>
 		</Section>
 	);
 }
-
-const skills = ['TypeScript', 'React', 'MUI', 'Java', 'Node.js', 'WordPress'];
