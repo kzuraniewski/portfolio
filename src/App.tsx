104f1c356216ef7bf2bb018bbd002e78cf543413
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-01-28 21:21:30 +0100

    redundant code removed

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,26 @@ import { ContactMe } from './components/ContactMe';
 import Container from './components/Container';
 import Footer from './components/Footer';
 import { Header } from './components/Header';
-import { AppContextProvider } from './lib/context';
 import { About, LandingPage, Projects } from './sections';
 
 export default function RootLayout() {
 	return (
 		<html lang="en" className="text-[18px]">
-			<AppContextProvider>
-				<body className="min-h-screen text-light bg-primary">
-					<Header />
+			<body className="min-h-screen text-light bg-primary">
+				<Header />
 
-					<main>
-						<Container className="py-10">
-							<LandingPage />
-							<About />
-							<Projects />
-						</Container>
+				<main>
+					<Container className="py-10">
+						<LandingPage />
+						<About />
+						<Projects />
+					</Container>
 
-						<ContactMe />
-					</main>
+					<ContactMe />
+				</main>
 
-					<Footer />
-				</body>
-			</AppContextProvider>
+				<Footer />
+			</body>
 		</html>
 	);
 }
