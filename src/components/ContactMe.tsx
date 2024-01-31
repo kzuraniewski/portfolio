21cd042b264fdbf21029b275c5c5346a7f23d77d
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-01-31 21:08:38 +0100

    contactme banner pointing to contact section

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,7 +1,7 @@
 export const ContactMe = () => {
 	return (
 		<a
-			href="mailto:zuraniewski.karol@gmail.com"
+			href="#contact"
 			className="fixed bottom-0 left-0 py-3 font-bold text-center uppercase rotate-45 -translate-x-1/2 hover:no-underline -translate-y-[50px] origin-[center_100px] w-96 bg-accent text-primary"
 		>
 			Contact me
