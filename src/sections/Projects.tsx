2955565ac4cb665bcd059417c08235d287747153
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-02-01 01:29:24 +0100

    priject highlights not centered fix

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -5,23 +5,25 @@ import { personalLinks, projects } from '@/lib/data';
 export default function Projects() {
 	return (
 		<Section title="My Projects" id="projects">
-			{projects
-				.filter((project) => project.featured)
-				.map((project, index) => (
-					<ProjectHighlight
-						project={project}
-						className={
-							'relative w-4/5 mx-auto ' +
-							(index % 2 ? '-translate-x-4' : 'translate-x-4')
-						}
-					/>
-				))}
+			<div className="flex flex-col items-center">
+				{projects
+					.filter((project) => project.featured)
+					.map((project, index) => (
+						<ProjectHighlight
+							project={project}
+							className={
+								'relative ' +
+								(index % 2 ? '-translate-x-4' : 'translate-x-4')
+							}
+						/>
+					))}
 
-			<div className="mx-auto w-fit">
-				See more projects on my{' '}
-				<a href={personalLinks.github} target="_blank">
-					GitHub
-				</a>
+				<div className="w-fit">
+					See more projects on my{' '}
+					<a href={personalLinks.github} target="_blank">
+						GitHub
+					</a>
+				</div>
 			</div>
 		</Section>
 	);
