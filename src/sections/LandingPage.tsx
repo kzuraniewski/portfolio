637fbeedc6dc3ccf3bf94d0b5cd4e289d7e497a7
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-02-01 19:41:46 +0100

    landing page double polygon

--- src/sections/LandingPage.tsx
+++ src/sections/LandingPage.tsx
@@ -19,18 +19,29 @@ export default function LandingPage() {
 			<Divider className="w-48 mb-7" />
 
 			<PolygonBackground
-				padding={20}
+				padding={[10, 20]}
+				background="accent"
 				getPoints={(width, height) => [
-					[5, 10],
-					[width, 0],
-					[width - 15, height],
-					[0, height - 10],
+					[45, 10],
+					[width - 10, 20],
+					[width - 70, height - 5],
+					[10, height - 30],
 				]}
 			>
-				<p className="max-w-sm text-center">
-					And I'm a fullstack web developer that specializes in
-					TypeScript, React and Java.
-				</p>
+				<PolygonBackground
+					padding={20}
+					getPoints={(width, height) => [
+						[5, 10],
+						[width, 0],
+						[width - 15, height],
+						[0, height - 10],
+					]}
+				>
+					<p className="max-w-sm text-center">
+						I'm a fullstack web developer that specializes in
+						TypeScript, React and Java.
+					</p>
+				</PolygonBackground>
 			</PolygonBackground>
 
 			<Button
