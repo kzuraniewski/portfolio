7eb370db48e89e2d39f6be93cf67c4f4f4e60c35
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-02-01 03:58:43 +0100

    about separated first paragraph

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,33 +1,33 @@
 import { CompanyHistory } from '@/components/CompanyHistory';
 import IconStack from '@/components/IconStack';
-import PolygonBackground, {
-	PolygonPointsFactory,
-} from '@/components/PolygonBackground';
+import PolygonBackground from '@/components/PolygonBackground';
 import { Section, SubSection } from '@/components/Section';
 import { learningIcons, mainTechIcons } from '@/lib/data';
-
-const getParagraphPoints: PolygonPointsFactory = (width, height) => [
-	[20, 10],
-	[width, 0],
-	[width - 35, height],
-	[0, height - 15],
-];
 
 export default function LandingPage() {
 	return (
 		<Section title="About me" id="about">
 			<PolygonBackground
-				className="w-2/3 mx-auto text-center"
-				getPoints={getParagraphPoints}
+				padding={[15, 25]}
+				className="mx-auto"
+				getPoints={(width, height) => [
+					[5, 10],
+					[width, 0],
+					[width - 15, height],
+					[0, height - 10],
+				]}
 			>
-				<p>
-					My name is Karol and I develop web stuff. My web development
-					journey started in 2021 - when I got to know React. Since
-					then I've had the opportunity to design, develop and
-					maintain websites, E-commerce services and fullstack web
-					applications at different&nbsp;companies.
+				<p className="text-center w-max">
+					Hi, my name is Karol. I develop web stuff.
 				</p>
 			</PolygonBackground>
+
+			<p className="w-2/3 mx-auto mt-8 text-center">
+				My web development journey started in 2021 - when I got to know
+				React. Since then I've had the opportunity to design, develop
+				and maintain websites, E-commerce services and fullstack web
+				applications at different companies.
+			</p>
 
 			<div className="flex justify-around">
 				<div className="flex flex-col">
