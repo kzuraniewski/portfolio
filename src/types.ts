867667acc67c70d4093ada300686288731705a8b
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-02-16 22:28:04 +0100

    docs(ui): jsdoc added

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,7 @@
+/**
+ * Defines props for a polymorphic component.
+ * Supplements base props with `as` and inherits from the specified component or element
+ */
 export type PolymorphicProps<
 	BaseProps extends Record<string, unknown>,
 	RootElement extends React.ElementType,
