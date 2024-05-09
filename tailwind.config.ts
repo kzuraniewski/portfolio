6174d5ecb4f30a3d1d11844dea6e98fa0a076eaf
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-05-09 11:12:11 +0200

    fix(ui): container max width limited to 1280px

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,6 +25,12 @@ export default {
 				DEFAULT: '1rem',
 				md: '2rem',
 			},
+			screens: {
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1280px',
+			},
 		},
 		fontFamily: {
 			display: ['Oswald', 'ui-sans-serif'],
