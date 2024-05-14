9f7189a0b9949820e1a48ebfa6b428aa1c45d156
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-05-14 13:44:37 +0200

    feat(ui): breakpoints updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,22 +12,16 @@ export default {
 				DEFAULT: '1rem',
 				md: '2rem',
 			},
-			screens: {
-				sm: '640px',
-				md: '768px',
-				lg: '1024px',
-				xl: '1280px',
-			},
 		},
 		fontFamily: {
 			display: ['Oswald', 'ui-sans-serif'],
 		},
 		screens: {
-			sm: '640px',
+			sm: '576px',
 			md: '768px',
-			lg: '1024px',
-			xl: '1280px',
-			'2xl': '1536px',
+			lg: '992px',
+			xl: '1200px',
+			'2xl': '1400px',
 		},
 		extend: {
 			colors: {
