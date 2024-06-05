36ae63d3b2a8dab2c3d627d8e448cd6686ae9bcd
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-06-06 01:43:34 +0200

    refactor(ui): footer migrated to tw

--- src/components/templates/Footer.tsx
+++ src/components/templates/Footer.tsx
@@ -1,18 +1,38 @@
 import { Trans } from '@lingui/macro';
 
+import tw from '@/lib/tw';
+
 export const Footer = () => {
 	return (
-		<footer className="border-t-2 border-dashed border-secondary text-center text-sm">
-			<div className="container py-8">
-				<a
+		<RootPanel>
+			<Content>
+				<Link
 					href="https://github.com/kzuraniewski"
-					className="text-on-primary hover:text-accent"
 					target="_blank"
 					rel="noreferrer"
 				>
 					<Trans>Designed and developed by Karol Żuraniewski</Trans>
-				</a>
-			</div>
-		</footer>
+				</Link>
+			</Content>
+		</RootPanel>
 	);
 };
+
+const RootPanel = tw.footer`
+	border-t-2
+	border-dashed
+	border-secondary
+	text-center
+	text-sm
+`;
+
+const Content = tw.div`
+	container
+	py-8
+`;
+
+const Link = tw.a`
+	text-on-primary
+
+	hover:text-accent
+`;
