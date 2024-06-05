445676d0197f98e0df459ee5a8085b79ce9b45a6
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-06-06 01:33:05 +0200

    refactor(ui): section migrated to tw

--- src/components/ui/Section.tsx
+++ src/components/ui/Section.tsx
@@ -1,7 +1,7 @@
 import { HTMLAttributes, useRef } from 'react';
-import cn from 'classnames';
 
 import { Divider } from '@/components/ui';
+import tw from '@/lib/tw';
 import useIntersectionObserver from '@/hooks/useIntersectionObserver';
 
 export type SectionProps = HTMLAttributes<HTMLDivElement> & {
@@ -14,7 +14,6 @@ export const Section = ({
 	title,
 	onViewportEnter,
 	onViewportExit,
-	className,
 	children,
 	...props
 }: SectionProps) => {
@@ -26,22 +25,54 @@ export const Section = ({
 	});
 
 	return (
-		<section
-			ref={rootRef}
-			className={cn('my-16 scroll-mt-32 xl:my-24', className)}
-			{...props}
-		>
-			<div className="container">
-				<div className="mb-16 flex items-center gap-8">
-					<h2 className="relative w-max font-display text-3xl font-bold uppercase">
-						<span className="text-accent">#</span> {title}
-					</h2>
+		<Root ref={rootRef} {...props}>
+			<Content>
+				<Header>
+					<Title>
+						<TitlePrefix>#</TitlePrefix> {title}
+					</Title>
 
-					<Divider className="mr-10 flex-grow" />
-				</div>
+					<Strip />
+				</Header>
 
 				{children}
-			</div>
-		</section>
+			</Content>
+		</Root>
 	);
 };
+
+const Root = tw.section`
+	my-16
+	scroll-mt-32
+
+	xl:my-24
+`;
+
+const Content = tw.div`
+	container
+`;
+
+const Header = tw.div`
+	mb-16
+	flex
+	items-center
+	gap-8
+`;
+
+const Title = tw.h2`
+	w-max
+	relative
+	font-display
+	text-3xl
+	font-bold
+	uppercase
+`;
+
+const TitlePrefix = tw.span`
+	text-accent
+`;
+
+const Strip = tw(Divider)`
+	mr-10
+	flex-grow
+`;
